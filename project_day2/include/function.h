c182d17b02740f50ddb1bb516446b6382b114754
huylaclac <nguyengialac29lequydon@gmail.com>
2025-02-12 16:00:27 +0700

    Add files via upload

---
+++ project_day2/include/function.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "datatype.h"
+
+
+void MainMenu();
+void displayAdminMenu();
+
+void displayUserInfor();
+void addUser(); 
+void searchUser(); 
+void end(); 
+
+void readUserFromFile(); 
+void saveAllUserToFile();
+
+ 
+
+#endif
+
