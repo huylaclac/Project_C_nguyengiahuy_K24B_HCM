d1eec503a64aef086784955b7070658b84abdb85
huylaclac <nguyengialac29lequydon@gmail.com>
2025-02-19 02:17:51 +0700

    Add files via upload

---
+++ FINAL_PROJECT_C_04/include/user.h
@@ -0,0 +1,16 @@
+// user.h chua nguyen mau ham  
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "datatype.h"
+
+int logUser();                 //                     
+void userMENU();
+
+void userInfor();
+void editUserInfor();
+void changePassword();
+
+
+#endif
