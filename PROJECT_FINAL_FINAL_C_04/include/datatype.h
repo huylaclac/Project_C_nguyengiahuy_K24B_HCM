59d7963e2d562095664df77fd25f9c1e6681476f
huylaclac <nguyengialac29lequydon@gmail.com>
2025-02-19 12:14:13 +0700

    Add files via upload

---
+++ PROJECT_FINAL_FINAL_C_04/include/datatype.h
@@ -0,0 +1,26 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+
+#define MAX_USERS 100
+#include <stdbool.h>
+
+typedef struct{
+	char id[15];
+	char name[30];
+	bool gender;
+	char phone[15];
+	char email[40];
+	char status[10];
+} User;
+
+
+typedef struct {
+    char username[50];
+    char password[50];
+} Admin;
+
+
+
+#endif
+
+
